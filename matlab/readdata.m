ace265224118da27553f27aca77663f6c152af44
DESKTOP-GKDBMJ2\BIBDR_ZXH <zhangnever001@gmail.com>
2019-01-11 17:23:51 +0800

    solve primal and dual problem using gurobi in matlab

--- matlab/readdata.m
+++ matlab/readdata.m
@@ -1,3 +1,27 @@
-A=csvread('A.csv');
-b=csvread('b.csv');
-c=csvread('c.csv');
+A = csvread('A.csv');
+b = csvread('b.csv');
+c = csvread('c.csv');
+
+m = 20;
+n = 100;
+
+cvx_solver gurobi
+cvx_begin
+    variable x(n);
+    minimize( c' * x );
+    subject to
+        A * x == b
+        x >= 0
+cvx_end
+
+x
+
+cvx_solver gurobi
+cvx_begin
+    variable y(m);
+    minimize( -b' * y );
+    subject to
+        A' * y - c <= 0
+cvx_end
+
+y
